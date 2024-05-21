ce8006aa2550a80e3f2721420c57c76798709d39
BernardoLF <bernardolfontes20@gmail.com>
2024-05-21 12:18:27 +0100

    API radio a funcionar

--- radio.js
+++ radio.js
@@ -1,3 +1,285 @@
+const radios = [
+    {
+        "_id": "3006059",
+        "_score": 343.16785,
+        "_source": {
+            "code": "PT",
+            "type": "country",
+            "title": "Portugal",
+            "url": "https://radio.garden/visit/portugal/lVedGqUL"
+        }
+    },
+    {
+        "_id": "3001673",
+        "_score": 119.35275,
+        "_source": {
+            "code": "PT",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Portugal Star",
+            "secure": false,
+            "url": "https://radio.garden/listen/portugal-star/nrigdCOx"
+        }
+    },
+    {
+        "_id": "3005921",
+        "_score": 101.5753,
+        "_source": {
+            "code": "PT",
+            "stream": "streamtheworld.com",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "RFM",
+            "secure": true,
+            "url": "https://radio.garden/listen/rfm/Ejy5Gaa6"
+        }
+    },
+    {
+        "_id": "3012617",
+        "_score": 101.5753,
+        "_source": {
+            "code": "PT",
+            "stream": "esradio.pt",
+            "subtitle": "Porto, Portugal",
+            "type": "channel",
+            "title": "ESRadioPT",
+            "secure": false,
+            "url": "https://radio.garden/listen/es-radio-pt/gDrTTOhl"
+        }
+    },
+    {
+        "_id": "3015358",
+        "_score": 101.5753,
+        "_source": {
+            "code": "PT",
+            "stream": "shoutca.st",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "SFTD",
+            "secure": false,
+            "url": "https://radio.garden/listen/sftd/O5BjlkNU"
+        }
+    },
+    {
+        "_id": "3038577",
+        "_score": 101.5753,
+        "_source": {
+            "code": "PT",
+            "stream": "radio.co",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Futura",
+            "secure": true,
+            "url": "https://radio.garden/listen/futura/qiExnFLt"
+        }
+    },
+    {
+        "_id": "3006731",
+        "_score": 97.74011,
+        "_source": {
+            "code": "PT",
+            "stream": "svrdedicado.org",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Radio Promove Portugal",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-promove-portugal/6BWED82w"
+        }
+    },
+    {
+        "_id": "3050423",
+        "_score": 96.07799,
+        "_source": {
+            "code": "PT",
+            "stream": "radioking.com",
+            "subtitle": "Porto, Portugal",
+            "type": "channel",
+            "title": "Veepradio.com",
+            "secure": true,
+            "url": "https://radio.garden/listen/veepradio-com/DsXbd0bq"
+        }
+    },
+    {
+        "_id": "3001458",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "justweb.pt",
+            "subtitle": "Porto, Portugal",
+            "type": "channel",
+            "title": "XL Radio",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-xl-fm-boa-musica/BtlHpHSi"
+        }
+    },
+    {
+        "_id": "3003174",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "yesstreaming.net",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Rádio Salesiana",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-salesiana/T9RxGYFT"
+        }
+    },
+    {
+        "_id": "3005258",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "rtp.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Antena 2",
+            "secure": true,
+            "url": "https://radio.garden/listen/antena-2/yChWrVUH"
+        }
+    },
+    {
+        "_id": "3009372",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "rtp.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Antena 1",
+            "secure": true,
+            "url": "https://radio.garden/listen/antena-1/kJZ26vKM"
+        }
+    },
+    {
+        "_id": "3011384",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "radioparanoia1000.com",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Rádio Paranóia",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-paranoia/TiIYPKTD"
+        }
+    },
+    {
+        "_id": "3012494",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "bauermedia.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Rádio Comercial",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-comercial/Fb7oz4pZ"
+        }
+    },
+    {
+        "_id": "3014035",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "rtp.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Rádio ZigZag",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-zigzag/lHdGrh31"
+        }
+    },
+    {
+        "_id": "3015128",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "rtp.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Rádio Lusitânia",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-lusitania/GVPMemP4"
+        }
+    },
+    {
+        "_id": "3015240",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "radios.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Radio Valdevez",
+            "secure": false,
+            "url": "https://radio.garden/listen/radiovaldevez/eas75MJd"
+        }
+    },
+    {
+        "_id": "3016243",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "rtp.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "RDP África",
+            "secure": true,
+            "url": "https://radio.garden/listen/rdp-africa/N2AzWRX9"
+        }
+    },
+    {
+        "_id": "3018019",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "rtp.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Antena 3",
+            "secure": true,
+            "url": "https://radio.garden/listen/antena-3/6lS0ncP9"
+        }
+    },
+    {
+        "_id": "3020182",
+        "_score": 95.78352,
+        "_source": {
+            "code": "PT",
+            "stream": "radios.pt",
+            "subtitle": "Lisbon, Portugal",
+            "type": "channel",
+            "title": "Rádio AMália",
+            "secure": true,
+            "url": "https://radio.garden/listen/radio-amalia/hlESAi9x"
+        }
+    }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const radioList = document.getElementById("radio-list");
+    const player = document.getElementById("player");
+    const radioTitle = document.getElementById("radio-title");
+    const radioFrame = document.getElementById("radio-frame");
+    const radioLinkContainer = document.getElementById("radio-link-container");
+    const radioLink = document.getElementById("radio-link");
+ 
+    radios.forEach(radio => {
+        const listItem = document.createElement("div");
+        listItem.className = "radio-item";
+        listItem.textContent = radio._source.title;
+        listItem.addEventListener("click", () => {
+            radioTitle.textContent = radio._source.title;
+            radioFrame.src = radio._source.url;
+            radioLink.href = radio._source.url;
+            radioLinkContainer.classList.remove("hidden");
+            player.classList.remove("hidden");
+        });
+        radioList.appendChild(listItem);
+    });
+});
+
 function voltarParaPaginaInicial() {
     window.location.href = "index.html";
 }
